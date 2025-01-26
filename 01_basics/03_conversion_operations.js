8d95ec256af5f9650223783e7f106117730a0ad8
Suryansh Sahu <168182355+suryanshsahu12@users.noreply.github.com>
2025-01-26 16:03:02 +0000

    conversions

---
+++ 01_basics/03_conversion_operations.js
@@ -0,0 +1,15 @@
+let score="32abc"
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);//be aware that above score is not a number ,but it can be converted into no.
+console.log( valueInNumber);
+
+//conversions
+//"33abc"->Nan
+//true->1
+
+//strings
+//booleon
+//""->false
+//"harshu"->true
