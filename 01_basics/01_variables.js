2be29ca3c38e44392ca7462398ff9809a29bda72
Suryansh Sahu <168182355+suryanshsahu12@users.noreply.github.com>
2025-01-26 10:50:39 +0000

    datatypes and varaiables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=1344
+let accountEmail="abhirandi@gmsail.com"
+var accountPassword="4342"
+accountcity="raibareli"
+
+
+//acccount_id=23
+accountEmail="sfifjhi@gmail"
+accountPassword="7347"
+console.log(accountId);
+let accountstate;
+/* don't use var*/ 
+console.table([accountId,accountEmail,accountPassword,accountstate])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,5 @@
+"use stricts";
+
+/*alert(3+3) we are using node.js not browser*/
+
+console.log(typeof"babu")
