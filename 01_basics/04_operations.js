984f31cf727a39d9bb334797dcd442073568648a
Suryansh Sahu <168182355+suryanshsahu12@users.noreply.github.com>
2025-01-26 17:16:21 +0000

    operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,25 @@
+let value =4
+let negValue=-value
+//console.log(negValue);
+// console.log(2-2);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+
+// let str1="abhi"
+// let str2=" is "
+// let str3=" rand"
+// let str4=str1+str2+str3
+// console.log(str4);
+
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log(1+"2"+2);
+// console.log(+true)
+// console.log(+"")
+let num1,num2
+
+
+
+
