0fd3e99b93877591db65fd7f3b1278e8852b83a6
Suryansh Sahu <168182355+suryanshsahu12@users.noreply.github.com>
2025-01-27 09:58:50 +0000

    time

---
+++ 01_basics/05_data_types.js
@@ -0,0 +1,37 @@
+// let name ="sury"
+// let repo= 6
+// console.log(`hello my name is ${name} and my repo count is ${repo}`);
+
+// const gameName= newString('teriluli')
+// console.log(gameName.charAt(2));//to find character place
+// console.log(gameName.indexOf('l'));//index of character
+
+
+// const newString = gameName.substring(0,4)
+// ----------------------------------------------------------------------------------
+
+
+// number and maths
+
+// const marksheet = new Number(11.234)
+// console.log(marksheet);
+// console.log(marksheet.toFixed(2));
+
+
+
+// crossOriginIsolated.log(Math.random());
+// console.log(Maths.floor(Math.random()*10)+1);
+
+// date ================================================================================
+
+// let myDate=newDate()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toTimeString());
+
+// newDate.toLocaleString('default',{weekday:"long"})
+
