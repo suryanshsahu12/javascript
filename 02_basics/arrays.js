8e3b703234813995b57e2b32bcae7a0b8c54b7f2
Suryansh Sahu <168182355+suryanshsahu12@users.noreply.github.com>
2025-01-27 17:26:08 +0000

    arrays

---
+++ 02_basics/arrays.js
@@ -0,0 +1,23 @@
+// const myArr =[0,1,2,3,4,5,6]
+
+
+// const myn1 = myArr.slice(2,4)
+
+// console.log(myn1);
+
+
+// const myn2= myArr.splice(2,5)
+// console.log(myn2)
+// console.log(myArr);
+
+
+const anotherArray = [1,2,3,4,5,[1,1,2,3],3,342,34,4234,1]
+const real_array=anotherArray.flat(Infinity)
+console.log(real_array);
+
+
+ let score1=100
+ let score2=200
+ let score3=300
+ console.log(Array.of(score1,score2,score3));
+ 
