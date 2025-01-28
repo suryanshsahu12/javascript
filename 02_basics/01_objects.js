5e22df8d5aa24286921382fb39c65e99d75f30d8
Suryansh Sahu <168182355+suryanshsahu12@users.noreply.github.com>
2025-01-28 16:33:54 +0000

    objects

---
+++ 02_basics/01_objects.js
@@ -0,0 +1,108 @@
+
+
+// const mysym = Symbol("mykey1")
+// const users={
+//     Name:"abhi randi",
+//     Email:"abhirandgoogle.com",
+//     location : "g.b.road",
+//     "loco":"suar rand",
+//     [mysym]: "mykey1"
+    
+// }
+// // console.log(users.Email);
+// // console.log(users["loco"]);
+// // Object.freeze(users)
+// // console.log(users[mysym]);
+
+// users.babu=function () {
+//     console.log("hello babu");
+    
+    
+// }
+
+// users.babu2=function () {
+//     console.log('hello babu',${this.Name});
+    
+    
+// // }
+// // users.babu();
+// // users.babu2();
+
+
+
+// //_______________________________________________________________________________________________________
+
+
+// const tinderuser={}
+// tinderuser.id="abesit@"
+// tinderuser.name="abhu"
+
+// // console.log(tinderuser);
+
+// const regularUser={
+//     email:"some@gmail.com",
+//     name:{ 
+//         userName:{
+//             randi:"abu_"
+//         }
+//    }
+// }
+
+// // console.log(regularUser.name);
+
+// const object1={
+//     1:"a",2:"b"
+// }
+
+// const object2={
+//     3:"c",4:'d'
+// }
+
+// // const obj3=Object.assign(object1,object2)  // Object.assign(target,source) or
+// // const obj4=Object.assign({},object1,object2)// here taget is empty and obj1 and obj2 are source
+// // console.log(obj4);
+
+// // // console.log(obj3);
+
+// // const user=[{email:"some@gmail.com",
+// //     name:{ 
+// //         userName:{
+// //             randi:"abu_"
+// //         }
+// //    }},{email:"some@gmail.com",
+// //     name:{ 
+// //         userName:{
+// //             randi:"abu_"
+// //         }
+// //    }}]
+// // const locy =user[1].email
+// // console.log(locy);
+
+
+// console.log(Object.keys(tinderuser)); // uses for printing keys in array format
+// console.log(Object.values(tinderuser));  // uses for printing "values" in array format
+// console.log(Object.entries(tinderuser)); // uses for converting each entry into a array[key,value ]
+
+
+// //--------------------------------------------------------------------------------------------------------
+
+//DESTRUCTURING
+// const course ={
+//     course_name:"javascript",
+//     courseinstructor:"hitesh",
+//     "fees":100
+
+
+// }
+// const{courseinstructor}=course  //destructuring
+// console.log(courseinstructor);
+
+
+
+// ___________________________________________________________________________________________________
+
+
+
+
+
+
