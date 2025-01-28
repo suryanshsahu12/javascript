f7b52378663f82bf606a334577377ee16e2bcf31
Suryansh Sahu <168182355+suryanshsahu12@users.noreply.github.com>
2025-01-28 19:55:16 +0000

    scopes

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,60 @@
+//to save time and energy we use functions
+
+// function new(){
+//     console.log(s);
+//     console.log(u);
+//     console.log(y);
+//     console.log(a);
+// }
+
+// new() ///definition
+
+// function addition(no1,no2) {
+//     // let result=no1 + no2
+//     return no1+no2
+//     }
+
+// const result=addition(34,85)
+
+// console.log("Result:",result);
+
+// function loginusermessage(username){
+//     return `$(username) just logged in`
+// }
+
+// console.log(loginusermessage("boy"));
+
+// //------------------------------------------------------------
+
+// function calculatetheSum(value1,value2,...num1){
+//     return num1
+// }
+// console.log(calculatetheSum(2,3,4,4));//the first two values are assigned to respective numbers and the rest (...num1)
+// shows the remaining value in an Array
+
+// const user ={
+//     name:"surya",
+//     price:23
+// }
+// function handleobject(anyobject){
+//     console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+
+// }
+
+// handleobject({
+//     username:"sam",
+//     price:399
+// })
+
+// const myArray=[200,3,4,5]
+// function returnArray(arrayvalue){
+//     return arrayvalue[2]
+// }
+// console.log(returnArray(myArray));
+
+
+//----------------------------------------------------------------
+
+
+
+
