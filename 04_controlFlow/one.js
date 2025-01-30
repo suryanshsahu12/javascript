8f14d0f39114cfa7d31293b14e89378857da9174
Suryansh Sahu <168182355+suryanshsahu12@users.noreply.github.com>
2025-01-30 17:22:58 +0000

    control flow

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,13 @@
+//var is global and let is local
+
+
+// const balance=35;
+// if (balance<30) {
+//     console.log("less condition");
+    
+    
+// }else if(balance<40){
+//     console.log("middle condition");
+    
+// }
+
