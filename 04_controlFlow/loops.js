7da8f8f940f41577afc2db7d981cacae1d104da7
Suryansh Sahu <168182355+suryanshsahu12@users.noreply.github.com>
2025-02-06 16:23:04 +0000

    loops

---
+++ 04_controlFlow/loops.js
@@ -0,0 +1,65 @@
+// const map=new Map()
+// map.set('IN','India')
+// map.set('Fr','France')
+// map.set('Tl','Thailand')
+// // console.log(map);
+
+// for (const key of map) {
+//     console.log(key);// for map we use forof and forin loop
+    
+    
+// }
+
+// for (const [key,value] of map) {
+//     console.log(key,':',value);
+    
+    
+// }
+
+// const myObjects={
+//     js:'javascript',
+//     cpp:'C++',
+//     rb:'ruby'
+// }
+// for (const key in myObjects) {
+//     console.log(`${key} value is ${myObjects[key]}`);// for objects and array we use forin loop
+    
+    
+// }
+
+
+// const programing=["js","rb","py","java"]
+// for (const key in programing) {
+//   console.log(programing[key]);
+  
+        
+//     }
+
+
+//     const map=new Map()
+//     map.set('IN','India')
+//     map.set('Fr','France')
+//     map.set('Tl','Thailand')
+//     for (const key in map) {
+//       console.log(key);
+      
+            
+        
+//     }
+
+const coding =["js","ruby","java","python","cpp"]
+// coding.forEach(function(val) {
+//     console.log(val);
+    
+    
+// });
+// coding.forEach( item)=> {
+//     console.log(item);
+    
+    
+// });
+
+function.printme(items) {
+    console.log(items);
+    
+}
