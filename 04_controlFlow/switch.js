b2e9503c5b02f29a669cfe76c5df53f02f499ba4
Suryansh Sahu <168182355+suryanshsahu12@users.noreply.github.com>
2025-02-06 16:23:26 +0000

    switch case

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,35 @@
+
+// const month=3
+
+// switch (month) {
+//     case 1 :
+//         console.log("january");
+        
+        
+//         break;
+//     case 2 :
+//         console.log("february");
+        
+//         break;
+//     case 3:
+//         console.log("March");
+        
+//         break;
+//     case 4:
+//         console.log("april");
+        
+//         break;
+
+
+//     default:
+//         console.log("may")
+// //         break;
+//     }
+
+// falsy values 
+// false ,0,-0, BigInt,0n,"",null,undefined,NaN
+
+// truthy values
+// "0",'false'," ",[],{},function(){}
+
+
