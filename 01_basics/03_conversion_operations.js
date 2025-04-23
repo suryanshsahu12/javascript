f3dff87a95713245b5e87afcfaa16d9f011f8e9c
Suryansh Sahu <168182355+suryanshsahu12@users.noreply.github.com>
2025-04-23 17:49:25 +0000

    dad

--- 01_basics/03_conversion_operations.js
+++ 01_basics/03_conversion_operations.js
@@ -4,7 +4,7 @@ console.log(typeof score);
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber);//be aware that above score is not a number ,but it can be converted into no.
 console.log( score);
-
+console.log(valueInNumber);
 //conversions
 //"33abc"->Nan
 //true->1
