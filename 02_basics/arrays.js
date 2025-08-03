16ceac5502c50d3a251dbabd31cb86f3fbf18324
Suryansh Sahu <168182355+suryanshsahu12@users.noreply.github.com>
2025-08-03 18:12:59 +0000

    conn

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -1,23 +1,32 @@
-// const myArr =[0,1,2,3,4,5,6]
+const myArr =[0,1,2,3,4,5,6]
 
-
+//myArr.push(7)
 // const myn1 = myArr.slice(2,4)
 
 // console.log(myn1);
 
-
+// myArr.unshift(9)
+// myArr.shift(9)
 // const myn2= myArr.splice(2,5)
 // console.log(myn2)
+//  console.log(myArr);
+// const myArr2 = myArr.splice(1,3)
+// console.log('b:',myArr2);
+// console.log(myArr2);
 // console.log(myArr);
 
 
-const anotherArray = [1,2,3,4,5,[1,1,2,3],3,342,34,4234,1]
-const real_array=anotherArray.flat(Infinity)
-console.log(real_array);
+// const anotherArray = [1,2,3,4,5,[1,1,2,3],3,342,34,4234,1]
+// const real_array=anotherArray.flat(Infinity)
+// console.log(real_array);
+//to addd two arrays we can use push(),concate()
+// and spread operator
+const myArr2 =[2,5,3,9,0]
+ const allarray = [...myArr,...myArr2]
+ console.log(allarray); 
 
-
- let score1=100
- let score2=200
- let score3=300
- console.log(Array.of(score1,score2,score3));
+//  let score1=100
+//  let score2=200
+//  let score3=300
+//  console.log(Array.of(score1,score2,score3));
  
