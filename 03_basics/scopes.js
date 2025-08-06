854f9f4dd20e68e9088fd1c39169814a533e932c
Suryansh Sahu <168182355+suryanshsahu12@users.noreply.github.com>
2025-08-06 05:43:54 +0000

    dd

--- 03_basics/scopes.js
+++ 03_basics/scopes.js
@@ -1,10 +1,10 @@
-// let a=10
-// const b=20
-// var c=30
+let a=10
+const b=20
+var c=30
 
-// console.log(a);
-// console.log(b);
-// console.log(c);
+console.log(a);
+console.log(b);
+console.log(c);
 
 
 // let a=100// global scope
